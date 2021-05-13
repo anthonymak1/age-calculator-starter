266ea36ed190ca5fe69e875d41bdd50edd57d73d
anthonymak1 <72530414+anthonymak1@users.noreply.github.com>
2021-05-12 17:47:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-$("button").click(function() {
-  let birthYear = $(".year").val();
-  let name = $('.name').val();
-  let age = 2020 - birthYear;
-$(".message").text(`${name} it looks like you are ${age} years old!`);
-});
+
